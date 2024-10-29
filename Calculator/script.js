ea5ba520bc96b4bffb2039f35772b75f9dd7463f
danplanned <elihcnad@gmail.com>
2024-10-28 21:29:29 -0700

    created onNumberPressed function in js file and implemented into html

---
+++ Calculator/script.js
@@ -0,0 +1,11 @@
+const resultText = document.getElementsByClassName("result-text")[0];
+
+function onNumberPressed{
+    if(number === "."){
+        if(resultText.innerHTML.length===0) return
+        if (returnText.innerHTML.includes(".")) return
+        
+    }
+
+    resultText.innerHTML += Number.toString();
+}
