3ee7cd11a03ad27bfb87548110e9281d5367d1d4
danplanned <elihcnad@gmail.com>
2024-10-30 01:44:42 -0700

    js logic completed

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,17 +1,61 @@
 const resultText = document.getElementsByClassName("result-text")[0];
 
-function onNumberPressed(number){
-    if(number === "."){
-        if(resultText.innerHTML.length===0 || resultText.innerHTML.includes("."))
-            return;
-        
-    }
+let storedValue = null;
+let operator = null;
 
-    resultText.innerHTML += number.toString();
+function onNumberPressed(number) {
+  if (number === ".") {
+    if (resultText.innerHTML.length === 0 || resultText.innerHTML.includes("."))
+      return;
+  }
+
+  resultText.innerHTML += number.toString();
 }
 
-function onOperationPressed(operation){
-    if (operation === "c"){
-        resultText.innerHTML = "";
-    }
+function canOperate() {
+  return resultText.innerHTML.length >= 1;
+}
+
+function operate(number) {
+  let result;
+
+  switch (operator) {
+    case "+":
+        result = storedValue + number;
+      break;
+
+    case "-":
+        result = storedValue - number;
+      break;
+
+    case "x":
+        result =  storedValue * number;
+      break;
+
+    case "/":
+        result =  storedValue / number;
+      break;
+  }
+
+  resultText.innerHTML = result;
+  storedValue = null;
+  operator = null;
+}
+
+function onOperationPressed(operation) {
+  const number = parseFloat(resultText.innerHTML);
+
+  if (!canOperate()) return;
+
+  if (operation === "=" && storedValue !== null) {
+    return operate(number);
+  } else if (operation === "c") {
+    storedValue = null;
+    operator = null;
+  } else {
+    operator = operation;
+    storedValue = number;
+  }
+
+  resultText.innerHTML = "";
 }
