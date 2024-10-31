26dc9a05f71536252bb6e77fcd4a2ec9d2837e2f
danplanned <elihcnad@gmail.com>
2024-10-30 21:26:11 -0700

    rock paper scissor is completed!

--- RockPaperScissor/script.js
+++ RockPaperScissor/script.js
@@ -1,11 +1,11 @@
-const HumanScore = 0;
-const ComputerScore = 0;
-const pcSelect = document.getElementById("pc-select")[0];
+let HumanScore = 0;
+let ComputerScore = 0;
 
-// rock:0 , paper:1 , scissors:2
-// rock beats scissors
-// scissors beats paper
-// paper beats rock
+const pcSelect = document.getElementById("pc-select");
+const humanSelect = document.getElementById("human-select");
+const humanScoreElement = document.getElementById("human-score");
+const computerScoreElement = document.getElementById("pc-score");
+const commentsElement = document.getElementById("comments");
 
 function randomOption() {
   let randomNum = Math.floor(Math.random() * 3);
@@ -23,55 +23,59 @@ function randomOption() {
 function onOptionPressed(option) {
   let selected = option;
   let compSelected = randomOption();
-  let feedBack = null;
+  let feedBack = "";
+
+  humanSelect.innerHTML = selected;
+  pcSelect.innerHTML = compSelected;
 
   switch (selected) {
-
     case "rock":
       switch (compSelected) {
         case "scissors":
           feedBack = "Player wins!";
+          HumanScore++;
           break;
         case "paper":
           feedBack = "Computer wins!";
+          ComputerScore++;
           break;
         default:
           feedBack = "It's a tie!";
       }
       break;
 
-      case "paper":
+    case "paper":
       switch (compSelected) {
         case "rock":
           feedBack = "Player wins!";
+          HumanScore++;
           break;
         case "scissors":
           feedBack = "Computer wins!";
+          ComputerScore++;
           break;
         default:
           feedBack = "It's a tie!";
       }
       break;
 
-      case "scissors":
+    case "scissors":
       switch (compSelected) {
         case "paper":
           feedBack = "Player wins!";
+          HumanScore++;
           break;
         case "rock":
           feedBack = "Computer wins!";
+          ComputerScore++;
           break;
         default:
           feedBack = "It's a tie!";
       }
       break;
-
   }
 
-
-
-
-
-
-  pcSelect.innerHTML = compSelected;
+  humanScoreElement.innerHTML = HumanScore;
+  computerScoreElement.innerHTML = ComputerScore;
+  commentsElement.innerHTML = feedBack;
 }
