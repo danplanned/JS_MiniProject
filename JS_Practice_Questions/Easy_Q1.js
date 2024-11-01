fe0580a48c69d60543edab3b0d42faa6b4e145f1
danplanned <elihcnad@gmail.com>
2024-11-01 11:50:39 -0700

    new folder for JS practice questions and easy Q1 script

---
+++ JS_Practice_Questions/Easy_Q1.js
@@ -0,0 +1,16 @@
+/**
+Write a function convertToFahrenheit that converts Celsius temperatures to Fahrenheit.
+The formula to convert Celsius to Fahrenheit is F = C * 9/5 + 32, where C is the
+temperature in Celsius and F is the temperature in Fahrenheit. 
+*/
+
+function convertToFahrenheit(celsius) {
+    let fahrenheit = celsius * (9/5) + 32;
+    console.log(fahrenheit);
+}
+
+convertToFahrenheit(0);
+convertToFahrenheit(-40);
+convertToFahrenheit(100);
+convertToFahrenheit(37);
+convertToFahrenheit(-19);
